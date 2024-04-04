810f3355bd28958765c198fb336726aaed066f2d
Cagey7 <dagodecagey@gmail.com>
2024-04-05 00:50:07 +0500

    initial structure

---
+++ fitness_api/settings/development.py
@@ -0,0 +1,16 @@
+from .base import *
+
+SECRET_KEY = "django-insecure-1t=jt8q4(-oaud+=_+4kkp)tm6^em-i@s^9ms+caoeg5z%x9%("
+ALLOWED_HOSTS = []
+DEBUG = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "fitness_api_development",
+        "USER": "postgres",
+        "PASSWORD": "123456",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
---
+++ fitness_api/settings/production.py
@@ -0,0 +1,18 @@
+import os
+from .base import *
+
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+ALLOWED_HOSTS = ["*"]
+DEBUG = False
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT"),
+    }
+}
