6c8a64d5fdf115af1fed3d99e41bdde7501312da
Cagey7 <dagodecagey@gmail.com>
2024-04-07 19:10:31 +0500

    added swagger

--- fitness_api/urls.py
+++ fitness_api/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/users/', include('users.urls')),
     path('api/v1/gym-manager/', include('gym_manager.urls')),
 ]
+
+urlpatterns += doc_urls
